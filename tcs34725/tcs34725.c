d8b22abd294ca8b90958b6a36253571d11361638
lee35210 <lee35210@gmail.com>
2020-03-19 15:03:24 +0900

    Update tcs34725.c

--- tcs34725/tcs34725.c
+++ tcs34725/tcs34725.c
@@ -133,7 +133,7 @@ ret_code_t tcs34725_set_wait_long(tcs34725_instance_t const * p_instance,
 {
     ret_code_t err_code;
     tcs34725_reg_data_t wait_long;
-    wait_long.reg_addr=TCS34725_REG_WAIT_TIME;
+    wait_long.reg_addr=TCS34725_REG_CONFIG;
     wait_long.reg_data=wait_long_val << TCS34725_WAIT_LONG_POS;
     err_code=tcs34725_write_reg(p_instance, &wait_long);
     return err_code;
